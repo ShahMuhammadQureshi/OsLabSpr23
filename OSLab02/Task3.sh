2f757a2e0ebd348bc678e338fd5ebab3cada62df
ShahMuhammadQureshi <123717044+ShahMuhammadQureshi@users.noreply.github.com>
2023-02-03 14:34:06 +0500

    Update Task3.sh

--- OSLab02/Task3.sh
+++ OSLab02/Task3.sh
@@ -1,10 +1,11 @@
+touch AllTime.txt
 for i in {1..5}
 do
 	cd Task$i
 	for j in {1..2}
 	do
 		cd 0$j
-		cat text.txt >> /home/student/Desktop/DFLAB/AllName.txt
+		cat text.txt >> /home/student/Desktop/DFLAB/AllTime.txt
 		cd ..
 	done
 cd ..
