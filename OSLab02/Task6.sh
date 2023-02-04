ca728102df0309dae4cac57a243a453a22424634
ShahMuhammadQureshi <123717044+ShahMuhammadQureshi@users.noreply.github.com>
2023-02-04 13:05:41 +0500

    Update Task6.sh

--- OSLab02/Task6.sh
+++ OSLab02/Task6.sh
@@ -1,15 +1,6 @@
-# Program for Fibonacci
-# Series
-
-# Static input for N
-N=6
-
-# First Number of the
-# Fibonacci Series
+echo "Enter the Number"
+read $N
 a=0
-
-# Second Number of the
-# Fibonacci Series
 b=1
 
 echo "The Fibonacci series is : "
@@ -21,4 +12,3 @@ do
 	a=$b
 	b=$fn
 done
-# End of for loop
